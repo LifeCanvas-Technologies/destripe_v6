065193f46e6a6e78baa99f82588c3def3d0da8c1
joegruss <106345744+joegruss@users.noreply.github.com>
2025-07-23 15:56:48 -0400

    Required run as administrator

--- install.bat
+++ install.bat
@@ -1,3 +1,10 @@
+@echo off
+if %cd% neq C:\Windows\System32 (
+	echo You need to run this installer as an Administrator
+	pause
+	exit
+)
+@echo on
 cd /D "%~dp0"
 call conda env remove -n cl_destripe_6
 call conda env remove -p C:\ProgramData\Anaconda3\envs\cl_destripe_6
